81c6139a35d27a6625d4a658b8243021503e08f6
chungongyu <chungongyu@gmail.com>
2021-09-29 13:58:35 +0800

    refactor: apply google coding style to toplevel py

--- evaluator.py
+++ evaluator.py
@@ -1,142 +1,211 @@
+"""Tools for inference, run
+		```bash
+		$python evaluator.py -h
+		```
+		for further help.
+"""
 import os
-import sys
 import argparse
 import logging
 import resource
 
-import numpy as np
 import torch
-from torch.utils.tensorboard import SummaryWriter
 from einops import rearrange
 
 # models & data
 from profold2 import constants
-from profold2.common import protein,residue_constants
-from profold2.data import esm,scn,custom
+from profold2.data import esm, scn, custom
 from profold2.data.utils import save_pdb
-from profold2.utils import Kabsch,TMscore
+from profold2.utils import Kabsch, TMscore
 
-def main(args):
-    # constants
-    DEVICE = constants.DEVICE # defaults to cuda if available, else cpu
-    
-    if args.threads > 0:
-        torch.set_num_threads(args.threads)
 
-    # set emebdder model from esm if appropiate - Load ESM-1b model
-    if args.features == "esm":
-        esm_extractor = esm.ESMEmbeddingExtractor(*esm.ESM_MODEL_PATH)
-    
-    if args.casp_version > 12:
-        test_loader = custom.load(
-            data_dir = args.casp_data,
-            feat_flags = ~custom.ProteinStructureDataset.FEAT_PDB,
-            batch_size = args.batch_size,
-            num_workers = 0)
-    else:
-        # get data
-        data = scn.load(
-            casp_version = args.casp_version,
-            thinning = args.casp_thinning,
-            batch_size = args.batch_size,
-            num_workers = 0,
-            filter_by_resolution = args.filter_by_resolution if args.filter_by_resolution > 0 else False,
-            dynamic_batching = False)
-        
-        print(data.keys())
-        test_loader = data[args.casp_data]
-    data_cond = lambda x: args.min_protein_len <= x['seq'].shape[1] and x['seq'].shape[1] < args.max_protein_len
+def main(args):  # pylint: disable=W0621
+  if args.threads > 0:
+    torch.set_num_threads(args.threads)
 
-    # model
-    model = torch.load(os.path.join(args.model))
-    model.eval()
-    model.to(DEVICE)
+  # set emebdder model from esm if appropiate - Load ESM-1b model
+  if args.features == 'esm':
+    esm_extractor = esm.ESMEmbeddingExtractor(*esm.ESM_MODEL_PATH)  # pylint: disable=W0612
 
-    tmscore, n = 0, 0
-    # eval loop
-    for i, batch in enumerate(filter(data_cond, iter(test_loader))):
-        if args.num_batches <= 0:
-            break
-        if args.pid and (not set(args.pid) & set(batch['pid'])):
-            continue
+  if args.casp_version > 12:
+    test_loader = custom.load(
+        data_dir=args.casp_data,
+        feat_flags=~custom.ProteinStructureDataset.FEAT_PDB,
+        batch_size=args.batch_size,
+        num_workers=0)
+  else:
+    # get data
+    data = scn.load(casp_version=args.casp_version,
+                    thinning=args.casp_thinning,
+                    batch_size=args.batch_size,
+                    num_workers=0,
+                    filter_by_resolution=args.filter_by_resolution
+                    if args.filter_by_resolution > 0 else False,
+                    dynamic_batching=False)
 
-        args.num_batches -= 1
+    test_loader = data[args.casp_data]
+  data_cond = lambda x: args.min_protein_len <= x['seq'].shape[1] and x['seq'].shape[1] < args.max_protein_len  # pylint: disable=C0301
 
-        logging.debug('seq.shape: {}'.format(batch['seq'].shape))
-    
-        with torch.no_grad():
-            # predict - out is (batch, L * 3, 3)
-            r = model(batch=batch, num_recycle=args.alphafold2_recycles)
-    
-            if 'folding' in r.headers:
-                assert 'coords' in r.headers['folding']
-                if 'coord' in batch:
-                    flat_cloud_mask = rearrange(batch['coord_mask'], 'b l c -> b (l c)')
+  # model
+  model = torch.load(os.path.join(args.model))
+  model.eval()
+  model.to(constants.DEVICE)
 
-                    # rotate / align
-                    coords_aligned, labels_aligned = Kabsch(
-                            rearrange(rearrange(r.headers['folding']['coords'], 'b l c d -> b (l c) d')[flat_cloud_mask], 'c d -> d c'), 
-                            rearrange(rearrange(batch['coord'], 'b l c d -> b (l c) d')[flat_cloud_mask], 'c d -> d c'))
-                    logging.debug('coords_aligned: {}'.format(coords_aligned.shape))
-                    logging.debug('labels_aligned: {}'.format(labels_aligned.shape))
-    
-                    tms = TMscore(rearrange(coords_aligned, 'd l -> () d l'), rearrange(labels_aligned, 'd l -> () d l'),
-                            L=torch.sum(batch['mask'], dim=-1))
-                    logging.info('{} pid: {} TM-score: {}'.format(i, ','.join(batch['pid']), tms.item()))
+  tmscore, n = 0, 0
+  # eval loop
+  for i, batch in enumerate(filter(data_cond, iter(test_loader))):
+    if args.num_batches <= 0:
+      break
+    if args.pid and (not set(args.pid) & set(batch['pid'])):
+      continue
 
-                    tmscore, n = tmscore + tms.item(), n + 1
+    args.num_batches -= 1
 
-                if args.save_pdb:
-                    save_pdb(i, batch, r.headers, os.path.join(args.prefix, 'pdbs'))
-            else:
-                raise ValueError('{} are not implemented yet!'.format(','.join(r.headers.keys())))
+    logging.debug('seq.shape: %s', batch['seq'].shape)
 
-    if n > 0:
-        logging.info('{} TM-score: {} (average)'.format(n, tmscore/n))
-    
+    with torch.no_grad():
+      # predict - out is (batch, L * 3, 3)
+      r = model(batch=batch, num_recycle=args.alphafold2_recycles)
+
+      if 'folding' in r.headers:
+        assert 'coords' in r.headers['folding']
+        if 'coord' in batch:
+          flat_cloud_mask = rearrange(batch['coord_mask'], 'b l c -> b (l c)')
+
+          # rotate / align
+          coords_aligned, labels_aligned = Kabsch(
+              rearrange(
+                  rearrange(r.headers['folding']['coords'],
+                            'b l c d -> b (l c) d')[flat_cloud_mask],
+                  'c d -> d c'),
+              rearrange(
+                  rearrange(batch['coord'],
+                            'b l c d -> b (l c) d')[flat_cloud_mask],
+                  'c d -> d c'))
+          logging.debug('coords_aligned: %s', coords_aligned.shape)
+          logging.debug('labels_aligned: %s', labels_aligned.shape)
+
+          tms = TMscore(rearrange(coords_aligned, 'd l -> () d l'),
+                        rearrange(labels_aligned, 'd l -> () d l'),
+                        L=torch.sum(batch['mask'], dim=-1))
+          logging.info('%d pid: %s TM-score: %f',
+              i, ','.join(batch['pid']), tms.item())
+
+          tmscore, n = tmscore + tms.item(), n + 1
+
+        if args.save_pdb:
+          save_pdb(i, batch, r.headers, os.path.join(args.prefix, 'pdbs'))
+      else:
+        raise ValueError('folding are not implemented yet!')
+
+  if n > 0:
+    logging.info('%d TM-score: %f (average)', n, tmscore / n)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-X', '--model', type=str, default='model.pth', help='model of alphafold2, default=\'model.pth\'')
-    parser.add_argument('--pid', type=str, action='append', help='pids to eval, default=\'\'')
-    parser.add_argument('-o', '--prefix', type=str, default='.', help='prefix of out directory, default=\'.\'')
-    parser.add_argument('-C', '--casp_version', type=int, default=12, help='CASP version, default=12')
-    parser.add_argument('-T', '--casp_thinning', type=int, default=30, help='CASP version, default=30')
-    parser.add_argument('-k', '--casp_data', type=str, default='test', help='CASP dataset, default=\'test\'')
-    parser.add_argument('-F', '--features', type=str, default='esm', help='AA residue features one of [esm,msa], default=esm')
-    parser.add_argument('-t', '--threads', type=int, default=0, help='number of threads used for intraop parallelism on CPU., default=0')
-    parser.add_argument('-m', '--min_protein_len', type=int, default=0, help='filter out proteins whose length<LEN, default=0')
-    parser.add_argument('-M', '--max_protein_len', type=int, default=1024, help='filter out proteins whose length>LEN, default=1024')
-    parser.add_argument('-r', '--filter_by_resolution', type=float, default=0, help='filter by resolution<=RES')
-    parser.add_argument('-n', '--num_batches', type=int, default=100000, help='number of batches, default=10^5')
-    parser.add_argument('-b', '--batch_size', type=int, default=1, help='batch size, default=1')
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-X',
+                      '--model',
+                      type=str,
+                      default='model.pth',
+                      help='model of alphafold2, default=\'model.pth\'')
+  parser.add_argument('--pid',
+                      type=str,
+                      action='append',
+                      help='pids to eval, default=\'\'')
+  parser.add_argument('-o',
+                      '--prefix',
+                      type=str,
+                      default='.',
+                      help='prefix of out directory, default=\'.\'')
+  parser.add_argument('-C',
+                      '--casp_version',
+                      type=int,
+                      default=12,
+                      help='CASP version, default=12')
+  parser.add_argument('-T',
+                      '--casp_thinning',
+                      type=int,
+                      default=30,
+                      help='CASP version, default=30')
+  parser.add_argument('-k',
+                      '--casp_data',
+                      type=str,
+                      default='test',
+                      help='CASP dataset, default=\'test\'')
+  parser.add_argument('-F',
+                      '--features',
+                      type=str,
+                      default='esm',
+                      help='AA residue features one of [esm,msa], default=esm')
+  parser.add_argument(
+      '-t',
+      '--threads',
+      type=int,
+      default=0,
+      help='number of threads used for intraop parallelism on CPU., default=0')
+  parser.add_argument('-m',
+                      '--min_protein_len',
+                      type=int,
+                      default=0,
+                      help='filter out proteins whose length<LEN, default=0')
+  parser.add_argument(
+      '-M',
+      '--max_protein_len',
+      type=int,
+      default=1024,
+      help='filter out proteins whose length>LEN, default=1024')
+  parser.add_argument('-r',
+                      '--filter_by_resolution',
+                      type=float,
+                      default=0,
+                      help='filter by resolution<=RES')
+  parser.add_argument('-n',
+                      '--num_batches',
+                      type=int,
+                      default=100000,
+                      help='number of batches, default=10^5')
+  parser.add_argument('-b',
+                      '--batch_size',
+                      type=int,
+                      default=1,
+                      help='batch size, default=1')
 
-    parser.add_argument('--alphafold2_recycles', type=int, default=0, help='number of recycles in alphafold2, default=0')
+  parser.add_argument('--alphafold2_recycles',
+                      type=int,
+                      default=0,
+                      help='number of recycles in alphafold2, default=0')
 
-    parser.add_argument('--save_pdb', action='store_true', help='save pdb files')
-    parser.add_argument('-v', '--verbose', action='store_true', help='verbose')
-    args = parser.parse_args()
+  parser.add_argument('--save_pdb', action='store_true', help='save pdb files')
+  parser.add_argument('-v', '--verbose', action='store_true', help='verbose')
+  args = parser.parse_args()
 
-    # logging
-    os.makedirs(os.path.abspath(args.prefix), exist_ok=True)
-    if args.save_pdb:
-        os.makedirs(os.path.abspath(os.path.join(args.prefix, 'pdbs')), exist_ok=True)
-    logging.basicConfig(
-            format = '%(asctime)-15s [%(levelname)s] (%(filename)s:%(lineno)d) %(message)s',
-            level = logging.DEBUG if args.verbose else logging.INFO,
-            handlers = [
-                logging.StreamHandler(),
-                logging.FileHandler(os.path.join(args.prefix, '{}.log'.format(
-                    os.path.splitext(os.path.basename(__file__))[0])))
-            ]
-        )
+  # logging
+  os.makedirs(os.path.abspath(args.prefix), exist_ok=True)
+  if args.save_pdb:
+    os.makedirs(os.path.abspath(os.path.join(args.prefix, 'pdbs')),
+                exist_ok=True)
+  logging.basicConfig(
+      format=
+      '%(asctime)-15s [%(levelname)s] (%(filename)s:%(lineno)d) %(message)s',
+      level=logging.DEBUG if args.verbose else logging.INFO,
+      handlers=[
+          logging.StreamHandler(),
+          logging.FileHandler(
+              os.path.join(
+                  args.prefix,
+									'{os.path.splitext(os.path.basename(__file__))[0]}.log'))
+      ])
 
-    logging.info('-----------------')
-    logging.info('Arguments: {}'.format(args))
-    logging.info('-----------------')
+  logging.info('-----------------')
+  logging.info('Arguments: %s', args)
+  logging.info('-----------------')
 
-    main(args)
+  main(args)
 
-    logging.info('-----------------')
-    logging.info('Resources(myself): {}'.format(resource.getrusage(resource.RUSAGE_SELF)))
-    logging.info('Resources(children): {}'.format(resource.getrusage(resource.RUSAGE_CHILDREN)))
-    logging.info('-----------------')
+  logging.info('-----------------')
+  logging.info('Resources(myself): %s',
+      resource.getrusage(resource.RUSAGE_SELF))
+  logging.info('Resources(children): %s',
+      resource.getrusage(resource.RUSAGE_CHILDREN))
+  logging.info('-----------------')
