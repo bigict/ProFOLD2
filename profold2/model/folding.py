fix: fp16 overflow

profold2/model/folding.py
@@ -12,7 +12,7 @@ from einops import rearrange, repeat
 from profold2.model.commons import init_zero_, embedd_dim_get
 from profold2.model.functional import (l2_norm, quaternion_multiply,
                                        quaternion_to_matrix, rigids_from_angles,
-                                       rigids_to_positions)
+                                       rigids_scale, rigids_to_positions)
 from profold2.utils import (default, exists,
                             torch_allow_tf32, torch_default_dtype)
 
@@ -381,12 +381,13 @@ class StructureModule(nn.Module):
                                   single_repr_init=single_repr_init)
           frames = rigids_from_angles(
               batch['seq'],
-              (rotations, translations*self.position_scale),
+              rigids_scale((rotations, translations), self.position_scale),
               l2_norm(angles))
+          frames = rigids_scale(frames, 1.0 / self.position_scale)
           coords = rigids_to_positions(frames, batch['seq'])
           coords.type(original_dtype)
           outputs.append(
-              dict(frames=(rotations, translations*self.position_scale),
+              dict(frames=(rotations, translations),
                    act=single_repr,
                    atoms=dict(frames=frames, coords=coords, angles=angles)))
 

profold2/model/head.py
@@ -488,12 +488,14 @@ class FoldingHead(nn.Module):
                fape_max=15,
                fape_z=15,
                dropout=.0,
+               position_scale=1.0,
                **params):
     super().__init__()
     self.struct_module = folding.StructureModule(dim,
                                                  structure_module_depth,
                                                  structure_module_heads,
-                                                 dropout=dropout)
+                                                 dropout=dropout,
+                                                 position_scale=position_scale)
 
     self.fape_min = fape_min
     self.fape_max = fape_max