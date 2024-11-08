6f32b62c3524cb1504489202d7de41ac5400b920
chungongyu <chungongyu@gmain.com>
2024-11-08 17:15:07 +0800

    refactor: relax on GPU works

--- install_env.sh
+++ install_env.sh
@@ -1,16 +1,24 @@
 set -e
 
 cuda_version=${cuda_version:-"12.1.1"}
-gcc_version=${gcc_version:-"11.2.0"}
+gcc_version=${gcc_version:-"12.4.0"}
+openmm_version=${openmm_version:-"8.0.0"}
 pytorch_version=${pytorch_version:-"2.3.1"}
 pytorch_cuda=$(echo ${cuda_version}|cut -d. -f1-2)
 
 # conda create -n pf2 python=3.11
 # conda activate pf2
 
+conda install -y -c conda-forge \
+    gxx=${gcc_version} \
+    ninja
+
+conda install -y -c conda-forge \
+    openmm=${openmm_version} \
+    pdbfixer
+
 conda install -y -c pytorch -c nvidia \
     pytorch=${pytorch_version} \
-    cuda-toolkit=${pytorch_cuda} \
     cuda-cccl=${pytorch_cuda} \
     pytorch-cuda=${pytorch_cuda}
 
@@ -21,7 +29,3 @@ conda install -y -c conda-forge \
     biopython \
     einops \
     tensorboard
-
-conda install -y -c conda-forge \
-    gxx=${gcc_version} \
-    ninja
